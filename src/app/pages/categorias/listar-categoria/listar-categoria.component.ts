97670c201e80f6c83fd5d97a06af523ac09bb40c
Víctor Cândido <victorcandido@MacBook-Air-de-Victor.local>
2020-02-09 01:01:55 -0300

    Implementação da lista de produtos

--- src/app/pages/categorias/listar-categoria/listar-categoria.component.ts
+++ src/app/pages/categorias/listar-categoria/listar-categoria.component.ts
@@ -23,7 +23,7 @@ export class ListarCategoriaComponent implements OnInit {
       { id: 3, nome_categoria: 'CATEGORIA 3' },
       { id: 4, nome_categoria: 'CATEGORIA 4' },
       { id: 5, nome_categoria: 'CATEGORIA 5' },
-    ]
+    ];
   }
 
   ngOnInit() {
--- src/app/pages/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/pages/produtos/listar-produtos/listar-produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoTableColumn } from '@portinari/portinari-ui';
 
 @Component({
   selector: 'app-listar-produtos',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-produtos.component.css']
 })
 export class ListarProdutosComponent implements OnInit {
+  public columns: PoTableColumn[];
+  public items: any[];
 
-  constructor() { }
+  constructor() {
+    this.columns = [
+      { label: 'ID', property: 'id' },
+      { label: 'Categoria', property: 'id_categoria' },
+      { label: 'Nome do produto', property: 'nome_produto' },
+    ];
+
+    this.items = [
+      { id: 1, id_categoria: 'CATEGORIA 1', nome_produto: 'PRODUTO 1' },
+      { id: 2, id_categoria: 'CATEGORIA 2', nome_produto: 'PRODUTO 2'},
+      { id: 3, id_categoria: 'CATEGORIA 3', nome_produto: 'PRODUTO 3' },
+      { id: 4, id_categoria: 'CATEGORIA 4', nome_produto: 'PRODUTO 4' },
+      { id: 5, id_categoria: 'CATEGORIA 5', nome_produto: 'PRODUTO 5' },
+    ];
+  }
 
   ngOnInit() {
   }
