e429e95dd1d09169bd9fb203848baa58af49ecd5
Víctor Cândido <victorcandido@MacBook-Air-de-Victor.local>
2020-02-09 21:02:26 -0300

    Implementação de produtos

--- src/app/pages/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/pages/produtos/listar-produtos/listar-produtos.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { PoTableColumn } from '@portinari/portinari-ui';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PoTableColumn, PoPageAction, PoTableComponent } from '@portinari/portinari-ui';
 
 @Component({
   selector: 'app-listar-produtos',
@@ -7,16 +7,24 @@ import { PoTableColumn } from '@portinari/portinari-ui';
   styleUrls: ['./listar-produtos.component.css']
 })
 export class ListarProdutosComponent implements OnInit {
-  public columns: PoTableColumn[];
   public items: any[];
 
-  constructor() {
-    this.columns = [
-      { label: 'ID', property: 'id' },
-      { label: 'Categoria', property: 'id_categoria' },
-      { label: 'Nome do produto', property: 'nome_produto' },
-    ];
+  public readonly actions: Array<PoPageAction> = [
+    { label: 'Novo', icon: '', action: this.novoFunction },
+    { label: 'Editar', icon: '', action: this.editarFunction },
+    { label: 'Salvar', icon: '', action: this.salvarFunction },
+    { label: 'Remover', icon: '', action: this.removerFunction },
+  ];
 
+  public readonly columns: PoTableColumn[] = [
+    { label: 'ID', property: 'id' },
+    { label: 'Categoria', property: 'id_categoria' },
+    { label: 'Nome do produto', property: 'nome_produto' },
+  ];
+
+  @ViewChild(PoTableComponent, { static: true }) poTable: PoTableComponent;
+
+  constructor() {
     this.items = [
       { id: 1, id_categoria: 'CATEGORIA 1', nome_produto: 'PRODUTO 1' },
       { id: 2, id_categoria: 'CATEGORIA 2', nome_produto: 'PRODUTO 2' },
@@ -27,6 +35,25 @@ export class ListarProdutosComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  novoFunction() {
+
+  }
+
+  editarFunction() {
+
+  }
+
+  salvarFunction() {
+
+  }
+
+  removerFunction() {
+    console.log(this.poTable.getSelectedRows());
+    const selectedRow = this.poTable.getSelectedRows()[0];
+
+    console.log(`Linha selecionada: ${selectedRow}`);
   }
 
 }
