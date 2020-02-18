e2833119be2f4b2837428e8a4439ea51ab3ed626
Víctor Cândido <victorcandido@MacBook-Air-de-Victor.local>
2020-02-17 22:10:27 -0300

    Corrigido bugs

--- src/app/pages/produtos/produtos-routing.module.ts
+++ src/app/pages/produtos/produtos-routing.module.ts
@@ -1,3 +1,4 @@
+import { CadastrarProdutosComponent } from './cadastrar-produtos/cadastrar-produtos.component';
 import { ListarProdutosComponent } from './listar-produtos/listar-produtos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'listar', component: ListarProdutosComponent },
+  { path: 'cadastrar', component: CadastrarProdutosComponent },
 ];
 
 @NgModule({
